52ab4468ba7d4f3e65b411b6f78c0e95fe64bdd7
Mini9709 <tjdals9709@naver.com>
2024-10-29 11:03:33 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 8.88 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,15 +1,12 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] seoul) {
         String answer = "";
-        int idx = 0;
         
-        for (String str : seoul) {
-            if (str.equals("Kim")) {
-                answer = "김서방은 " + idx + "에 있다";
-            } else {
-                idx += 1;
-            }
-        }
+        int idx = Arrays.asList(seoul).indexOf("Kim");
+        
+        answer = "김서방은 " + idx + "에 있다";
         
         return answer;
     }

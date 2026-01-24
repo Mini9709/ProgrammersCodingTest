006ae4ce711e4afa47bfac024fae92ab855f8152
Mini9709 <tjdals9709@naver.com>
2026-01-24 12:27:42 +0900

    [Silver III] Title: 수열, Time: 252 ms, Memory: 23848 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+        int answer = Integer.MIN_VALUE;
+
+        for (int i = 1; i <= n; i++) {
+            sum += arr[i];
+            if (i >= k) {
+                answer = Math.max(answer, sum);
+                sum -= arr[i-k+1];
+            }
+        }
+
+        System.out.println(answer);
+    }
+}

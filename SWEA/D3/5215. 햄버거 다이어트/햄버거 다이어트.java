f69ecebf33fff7050e82710b47962776ca6e7a11
Mini9709 <tjdals9709@naver.com>
2026-02-06 22:06:03 +0900

    [D3] Title: 햄버거 다이어트, Time: 130 ms, Memory: 35,540 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -2,47 +2,42 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
-
+ 
     public static void main(String[] args) throws IOException {
-
+ 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
+ 
         int testCase = Integer.parseInt(br.readLine());
-
+ 
         for (int t = 1; t <= testCase; t++) {
             st = new StringTokenizer(br.readLine());
-
+ 
             int n = Integer.parseInt(st.nextToken());
             int l = Integer.parseInt(st.nextToken());
             int[][] arr = new int[n][2];
-
+ 
             for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(br.readLine());
                 arr[i][0] = Integer.parseInt(st.nextToken());
                 arr[i][1] = Integer.parseInt(st.nextToken());
             }
-
-            int answer = 0;
-
-            for (int i = 0; i < 1 << n; i++) {
-                int sumKcal = 0;
-                int sumValue = 0;
-                for (int j = 0; j < n; j++) {
-                    if ((i & 1 << j) > 0) {
-                        sumValue += arr[j][0];
-                        sumKcal += arr[j][1];
+ 
+            int[][] dp = new int[n+1][l+1];
+ 
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= l; j++) {
+                    if (arr[i-1][1] <= j) {
+                        dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-arr[i-1][1]] + arr[i-1][0]);
+                    } else {
+                        dp[i][j] = dp[i-1][j];
                     }
                 }
-
-                if (sumKcal <= l) {
-                    answer = Math.max(answer, sumValue);
-                }
             }
-
-            System.out.println("#" + t + " " + answer);
+ 
+            System.out.printf("#%d %d\n", t, dp[n][l]);
         }
     }
 }

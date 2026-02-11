60f960f02d4d6037fef5dea5b33c245c19eba176
Mini9709 <tjdals9709@naver.com>
2026-02-11 16:26:35 +0900

    [D4] Title: 의석이의 우뚝 선 산, Time: 200 ms, Memory: 31,232 KB -BaekjoonHub

--- SWEA/D4/4796. 의석이의 우뚝 선 산/의석이의 우뚝 선 산.java
+++ SWEA/D4/4796. 의석이의 우뚝 선 산/의석이의 우뚝 선 산.java
@@ -1,28 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
 import java.util.Scanner;
+
+/*
+ * 문제 : 의석이의 우뚝 선 산 (BOJ 4796)
+ * 메모리 : 101,896 kb
+ * 실행시간 : 654 ms
+ * 시간 복잡도 : O(n)
+ * 아이디어 :
+ * 	앞에서부터 다음 산과의 높이를 확인하여 우뚝 선 산 구간을 체크
+ */
 
 public class Solution {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Scanner in = new Scanner(System.in);
+		StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
 		
-		int testCase = in.nextInt();
+		st.nextToken();
+		int testCase = (int) st.nval;
 		
 		for (int t = 1; t <= testCase; t++) {
-			int n = in.nextInt();
+			st.nextToken();
+			int n = (int) st.nval;
 			int answer = 0;
 			
 			int[] mountains = new int[n];
 			for (int i = 0; i < n; i++) {
-				mountains[i] = in.nextInt();
+				st.nextToken();
+				mountains[i] = (int) st.nval;
 			}
+				
+			/*
+			 * 1. for문 시작
+			 * 2. 최대로 긴 우뚝 선 산 구간을 계산
+			 * 3. 우뚝 선 산 구간의 왼쪽 산 개수 * 오른쪽 산 개수 = 해당 구간의 우뚝 선 산 구간 개수
+			 * 4. for문 종료 후 answer값 리턴
+			 */
 			
-			
-			// 앞에서부터 for문 시작
-			// 올라가면 plus++
-			// 내려가면 minus--
-			// 다시 올라가면 초기화 후 plus++
-			// 초기화 전 answer += plus * minus
 			int plus = 0;
 			int minus = 0;
 			int temp = mountains[0];

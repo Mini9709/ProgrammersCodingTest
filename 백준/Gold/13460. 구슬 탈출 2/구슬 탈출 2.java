37d6f900c14d58b96b4d473d1bbd3057037d4c42
Mini9709 <tjdals9709@naver.com>
2026-02-27 17:44:11 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 104 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -14,172 +14,8 @@ public class Main {
 	static int bx;
 	static int by;
 	static int answer;
-	static int[][] map; // 벽 : -1, 빈 공간 : 0, 빨간 공 : 1, 파란 공 : 2
-	
-	// 0 : 상, 1 : 하, 2 : 좌, 3 : 우
-	public static void move(int mv, int[][] temp, int[][] p) {
-		
-		Queue<int[]> q = new ArrayDeque<>();
-		
-		if (mv == 0) {
-			// 빨간 공, 파란 공 queue에 넣기 (먼저 수행할 공 순서 정하기)
-			for (int y = 0; y < n; y++) {
-				if (temp[y][p[0][0]] == 1 || temp[y][p[0][0]] == 2) {
-					q.add(new int[] {p[0][0], y, temp[y][p[0][0]]}); // x, y, 공 색깔
-					temp[y][p[0][0]] = '.';
-				}
-				
-				if (temp[y][p[1][0]] == 1 || temp[y][p[1][0]] == 2) {
-					q.add(new int[] {p[1][0], y, temp[y][p[1][0]]});
-					temp[y][p[1][0]] = '.';
-				}
-			}
-			
-			// 공 움직이기
-			while (!q.isEmpty()) {
-				int[] curr = q.poll();
-				
-				for (int y = curr[1]; y >= 0; y--) {
-					if (temp[y][curr[0]] == -1 || temp[y][curr[0]] == 1 || temp[y][curr[0]] == 2) {
-						p[curr[2]-1][1] = y+1;
-						temp[y+1][curr[0]] = curr[2];
-						break;
-					} else if (temp[y][curr[0]] == 3) {
-						p[curr[2]-1][1] = -1;
-						p[curr[2]-1][0] = -1;
-						break;
-					}
-				}
-			}
-		} else if (mv == 1) {
-			// 빨간 공, 파란 공 queue에 넣기 (먼저 수행할 공 순서 정하기)
-			for (int y = n-1; y >= 0; y--) {
-				if (temp[y][p[0][0]] == 1 || temp[y][p[0][0]] == 2) {
-					q.add(new int[] {p[0][0], y, temp[y][p[0][0]]}); // x, y, 공 색깔
-					temp[y][p[0][0]] = '.';
-				}
-				
-				if (temp[y][p[1][0]] == 1 || temp[y][p[1][0]] == 2) {
-					q.add(new int[] {p[1][0], y, temp[y][p[1][0]]});
-					temp[y][p[1][0]] = '.';
-				}
-			}
-			
-			// 공 움직이기
-			while (!q.isEmpty()) {
-				int[] curr = q.poll();
-				
-				for (int y = curr[1]; y < n; y++) {
-					if (temp[y][curr[0]] == -1 || temp[y][curr[0]] == 1 || temp[y][curr[0]] == 2) {
-						p[curr[2]-1][1] = y-1;
-						temp[y-1][curr[0]] = curr[2];
-						break;
-					} else if (temp[y][curr[0]] == 3) {
-						p[curr[2]-1][1] = -1;
-						p[curr[2]-1][0] = -1;
-						break;
-					}
-				}
-			}			
-		} else if (mv == 2) {
-			// 빨간 공, 파란 공 queue에 넣기 (먼저 수행할 공 순서 정하기)
-			for (int x = 0; x < m; x++) {
-				if (temp[p[0][1]][x] == 1 || temp[p[0][1]][x] == 2) {
-					q.add(new int[] {x, p[0][1], temp[p[0][1]][x]}); // x, y, 공 색깔
-					temp[p[0][1]][x] = '.';
-				}
-				
-				if (temp[p[1][1]][x] == 1 || temp[p[1][1]][x] == 2) {
-					q.add(new int[] {x, p[1][1], temp[p[1][1]][x]});
-					temp[p[1][1]][x] = '.';
-				}
-			}
-			
-			// 공 움직이기
-			while (!q.isEmpty()) {
-				int[] curr = q.poll();
-				
-				for (int x = curr[0]; x >= 0; x--) {
-					if (temp[curr[1]][x] == -1 || temp[curr[1]][x] == 1 || temp[curr[1]][x] == 2) {
-						p[curr[2]-1][0] = x+1;
-						temp[curr[1]][x+1] = curr[2];
-						break;
-					} else if (temp[curr[1]][x] == 3) {
-						p[curr[2]-1][1] = -1;
-						p[curr[2]-1][0] = -1;
-						break;
-					}
-				}
-			}
-		} else {
-			// 빨간 공, 파란 공 queue에 넣기 (먼저 수행할 공 순서 정하기)
-			for (int x = m-1; x >= 0; x--) {
-				if (temp[p[0][1]][x] == 1 || temp[p[0][1]][x] == 2) {
-					q.add(new int[] {x, p[0][1], temp[p[0][1]][x]}); // x, y, 공 색깔
-					temp[p[0][1]][x] = '.';
-				}
-				
-				if (temp[p[1][1]][x] == 1 || temp[p[1][1]][x] == 2) {
-					q.add(new int[] {x, p[1][1], temp[p[1][1]][x]});
-					temp[p[1][1]][x] = '.';
-				}
-			}
-			
-			// 공 움직이기
-			while (!q.isEmpty()) {
-				int[] curr = q.poll();
-				
-				for (int x = curr[0]; x < m; x++) {
-					if (temp[curr[1]][x] == -1 || temp[curr[1]][x] == 1 || temp[curr[1]][x] == 2) {
-						p[curr[2]-1][0] = x-1;
-						temp[curr[1]][x-1] = curr[2];
-						break;
-					} else if (temp[curr[1]][x] == 3) {
-						p[curr[2]-1][1] = -1;
-						p[curr[2]-1][0] = -1;
-						break;
-					}
-				}
-			}
-		}
-	}
-	
-	public static void dfs(int c, int[] seq) {
-		if (c == 10) {
-			int[][] temp = new int[n][m];
-			int[][] point = {{rx, ry}, {bx, by}};
-			int count = 1;
-			
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
-					temp[i][j] = map[i][j];
-				}
-			}
-			
-			for (int s : seq) {
-				move(s, temp, point);
-				if (point[1][0] == -1 && point[1][1] == -1) {
-					return;
-				} else if (point[0][0] == -1 && point[0][1] == -1) {
-					answer = Math.min(answer, count);
-					return;
-				}
-				
-				count++;
-			}
-			
-			return;
-		}
-		
-		for (int i = 0; i < 4; i++) {
-			if (answer == 1) {
-				return;
-			}
-			
-			seq[c] = i;
-			dfs(c+1, seq);
-		}
-	}
+	static char[][] map; // 벽 : -1, 빈 공간 : 0, 빨간 공 : 1, 파란 공 : 2
+	static int[][] directions = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 	
 	public static void main(String[] args) throws IOException {
 
@@ -188,50 +24,127 @@ public class Main {
 		
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		map = new int[n][m];
+		map = new char[n][m];
 		
 		for (int i = 0; i < n; i++) {
 			String str = br.readLine();
 			
 			for (int j = 0; j < m; j++) {
-				char c = str.charAt(j);
+				map[i][j] = str.charAt(j);
 				
-				if (c == 'R') {
+				if (map[i][j] == 'R') {
 					rx = j;
 					ry = i;
-					map[i][j] = 1;
+					map[i][j] = '.';
 				}
 				
-				else if (c == 'B') {
+				else if (map[i][j] == 'B') {
 					bx = j;
 					by = i;
-					map[i][j] = 2;
+					map[i][j] = '.';
 				}
+			}
+		}
+		
+		Queue<int[]> q = new ArrayDeque<>();
+		boolean[][][][] visited = new boolean[n][m][n][m];
+		q.add(new int[] {rx, ry, bx, by});
+		visited[ry][rx][by][bx] = true;
+	
+		int time = 1;
+		answer = -1;
+		
+		while (time <= 10) {
+			int size = q.size();
+			
+			for (int i = 0; i < size; i++) {
+				int[] curr = q.poll();
+				int x1 = curr[0];
+				int y1 = curr[1];
+				int x2 = curr[2];
+				int y2 = curr[3];
 				
-				else if (c == '#') {
-					map[i][j] = -1;
-				}
-				
-				else if (c == '.') {
-					map[i][j] = 0;
-				} else {
-					map[i][j] = 3;
+				for (int[] d : directions) {
+					int i1 = 1;
+					int i2 = 1;
+					int nx1 = 0; int ny1 = 0; int nx2 = 0; int ny2 = 0;
+					
+					// 빨간 공 배치
+					while (true) {
+						nx1 = x1 + i1*d[0];
+						ny1 = y1 + i1*d[1];
+						
+						if (nx1 >= 0 && nx1 < m && ny1 >= 0 && ny1 < n) {
+							if (map[ny1][nx1] == '#') {
+								nx1 = nx1 - d[0];
+								ny1 = ny1 - d[1];
+								break;
+							} else if (map[ny1][nx1] == 'O') {
+								nx1 = -1;
+								ny1 = -1;
+								break;
+							}
+							
+							i1++;
+						} else {
+							break;
+						}
+					}
+
+					// 파란 공 배치
+					while (true) {
+						nx2 = x2 + i2*d[0];
+						ny2 = y2 + i2*d[1];
+						
+						if (nx2 >= 0 && nx2 < m && ny2 >= 0 && ny2 < n) {
+							if (map[ny2][nx2] == '#') {
+								nx2 = nx2 - d[0];
+								ny2 = ny2 - d[1];
+								break;
+							} else if (map[ny2][nx2] == 'O') {
+								nx2 = -1;
+								ny2 = -1;
+								break;
+							}
+							
+							i2++;
+						} else {
+							break;
+						}
+					}
+					
+					// 빨간 공과 파란 공에 따른 상황 설정
+					if (nx2 == -1 && ny2 == -1) {
+						continue;
+					} else if (nx1 == -1 && ny1 == -1) {
+						answer = time;
+						break;
+					} else if (nx1 == nx2 && ny1 == ny2) {
+						if (i1 > i2) {
+							nx1 -= d[0];
+							ny1 -= d[1];
+						} else {
+							nx2 -= d[0];
+							ny2 -= d[1];
+						}
+					}
+					
+					// 설정 후 방문여부 확인
+					if (!visited[ny1][nx1][ny2][nx2]) {
+						visited[ny1][nx1][ny2][nx2] = true;
+						q.add(new int[] {nx1, ny1, nx2, ny2});
+					}
 				}
 			}
 
-		}
-		
-		answer = Integer.MAX_VALUE;
-		int[] seq = new int[10];
-		
-		dfs(0, seq);
-		
-		if (answer == Integer.MAX_VALUE) {
-			answer = -1;
+			// 위치 선정 후 정답일 경우
+			if (answer != -1) {
+				break;
+			}
+			
+			time++;
 		}
 		
 		System.out.println(answer);
-		
 	}
-
 }

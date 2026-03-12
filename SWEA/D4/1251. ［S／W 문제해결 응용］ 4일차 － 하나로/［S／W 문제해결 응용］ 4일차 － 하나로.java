a50bd830eecc842d2b1634dbbbb308b6b4358914
Mini9709 <tjdals9709@naver.com>
2026-03-12 13:33:18 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 906 ms, Memory: 94,948 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -4,55 +4,16 @@ import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-class Edge implements Comparable<Edge> {
-
-	int i;
-	int j;
-	long cost;
-	
-	public Edge(int i, int j, long cost) {
-		this.i = i;
-		this.j = j;
-		this.cost = cost;
-	}
-	
-	@Override
-	public int compareTo(Edge o) {
-		return Long.compare(this.cost, o.cost);
-	}
-	
-}
-
 public class Solution {
 
 	static int n;
-	static int[] parent;
-	static int[][] island;
-	static long answer;
 	static double e;
+	static long[][] islands;
+	static boolean[] visited;
+	static long answer;
 	
-	public static int find(int x) {
-		if (parent[x] == x) return x;
-		else return parent[x] = find(parent[x]);
-	}
-	
-	public static void union(int x, int y) {
-		x = find(x);
-		y = find(y);
-		
-		if (x <= y) parent[y] = x;
-		else parent[x] = y;
-	}
-	
-	public static void kruscal(int x, int y, long cost) {
-		if (find(x) != find(y)) {
-			answer += cost;
-			union(x, y);
-		}
-	}
-	
-	public static void main(String[] args) throws IOException {
-	   	 
+    public static void main(String[] args) throws IOException {
+    	
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         
@@ -60,45 +21,43 @@ public class Solution {
         
         for (int t = 1; t <= testCase; t++) {
         	n = Integer.parseInt(br.readLine());
-        	
-        	island = new int[n][2];
+        	islands = new long[n][2];
+        	visited = new boolean[n];
+        	PriorityQueue<long[]> pq = new PriorityQueue<>((o1, o2) -> Long.compare(o1[0], o2[0]));
         	
         	st = new StringTokenizer(br.readLine());
         	for (int i = 0; i < n; i++) {
-        		island[i][0] = Integer.parseInt(st.nextToken());
+        		islands[i][0] = Long.parseLong(st.nextToken());
         	}
         	
         	st = new StringTokenizer(br.readLine());
         	for (int i = 0; i < n; i++) {
-        		island[i][1] = Integer.parseInt(st.nextToken());
-        	}
-        	
-        	parent = new int[n];
-        	
-        	for (int i = 0; i < n; i++) {
-        		parent[i] = i;
+        		islands[i][1] = Long.parseLong(st.nextToken());
         	}
         	
         	e = Double.parseDouble(br.readLine());
-        	PriorityQueue<Edge> pq = new PriorityQueue<>();
         	
         	answer = 0;
-        	
-        	for (int i = 0; i < n-1; i++) {
-        		for (int j = i+1; j < n; j++) {
-        			long lenX = Math.abs((island[i][0]-island[j][0]));
-        			long lenY = Math.abs((island[i][1]-island[j][1]));
-        			pq.add(new Edge(i, j, lenX*lenX + lenY*lenY));
-        		}
-        	}
+        	pq.add(new long[] {0, 0});
         	
         	while (!pq.isEmpty()) {
-        		Edge curr = pq.poll();
+        		long[] curr = pq.poll();
+        		int currI = (int) curr[1];
         		
-        		kruscal(curr.i, curr.j, curr.cost);
+        		if (visited[currI]) continue;
+        		
+        		answer += curr[0];
+        		visited[currI] = true;
+    			for (int i = 0; i < n; i++) {
+    				if (!visited[i]) {
+    					long x = Math.abs(islands[currI][0] - islands[i][0]);
+            			long y = Math.abs(islands[currI][1] - islands[i][1]);
+            			pq.add(new long[]{(x*x + y*y), i});
+    				}
+    			}
         	}
         	
-        	answer = Math.round(answer*e);
+        	answer = Math.round(answer * e);
         	
         	System.out.println("#" + t + " " + answer);
         }

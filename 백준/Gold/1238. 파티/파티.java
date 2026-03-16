f55bc910fa512a013a3eb08f94856cfc3671a8a1
Mini9709 <tjdals9709@naver.com>
2026-03-16 14:28:07 +0900

    [Gold III] Title: 파티, Time: 180 ms, Memory: 19480 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -2,117 +2,99 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main {
-
-	static class Node implements Comparable<Node> {
-
-		int start;
-		int end;
-		int time;
-
-		public Node(int start, int end, int time) {
-			this.start = start;
-			this.end = end;
-			this.time = time;
-		}
-
-		@Override
-		public int compareTo(Node o) {
-
-			return time - o.time;
-		}
-
-	}
-
+public class Main{
+	
+	static int n;
+	static int m;
+	static int x;
+	static int answer;
+	static int[] go;
+	static int[] back;
+	static ArrayList<int[]>[] route1;
+	static ArrayList<int[]>[] route2;
+	
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws IOException {
-
-		ArrayList<ArrayList<Node>> road_graph;
-		int answer = 0;
-
+		//------여기에 솔루션 코드를 작성하세요.------------
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int students = Integer.parseInt(st.nextToken());
-		int roads = Integer.parseInt(st.nextToken());
-		int destination = Integer.parseInt(st.nextToken());
-
-		road_graph = new ArrayList<>();
-
-		for (int i = 0; i <= students; i++) {
-
-			road_graph.add(new ArrayList<>());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		x = Integer.parseInt(st.nextToken());
+		route1 = new ArrayList[n+1];
+		route2 = new ArrayList[n+1];
+		
+		for (int i = 1; i <= n; i++) {
+			route1[i] = new ArrayList<>();
+			route2[i] = new ArrayList<>();
 		}
-
-		for (int i = 0; i < roads; i++) {
-
+		
+		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
+			
+			int prev = Integer.parseInt(st.nextToken());
+			int next = Integer.parseInt(st.nextToken());
 			int time = Integer.parseInt(st.nextToken());
-
-			Node n = new Node(start, end, time);
-			road_graph.get(start).add(n);
-			road_graph.get(end).add(n);
+			
+			route1[prev].add(new int[] {next, time});
+			route2[next].add(new int[] {prev, time});
 		}
+		
+		answer = 0;
 
-		int[] dp_go = new int[students + 1];
-		int[] dp_come = new int[students + 1];
-		for (int i = 0; i < dp_go.length; i++) {
-			dp_go[i] = Integer.MAX_VALUE;
-			dp_come[i] = Integer.MAX_VALUE;
-		}
-
-		boolean[] visited = new boolean[students + 1];
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-
-		Node start_points = new Node(destination, destination, 0);
-		dp_go[destination] = 0;
-		dp_come[destination] = 0;
-		pq.offer(start_points);
-
+		go = new int[n+1];
+		back = new int[n+1];
+		
+		Arrays.fill(go, Integer.MAX_VALUE);
+		Arrays.fill(back, Integer.MAX_VALUE);
+		
+		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
+		pq.add(new int[] {x, 0});
+        go[x] = 0;
+		back[x] = 0;
+		
 		while (!pq.isEmpty()) {
-
-			Node curr_node = pq.poll();
-			ArrayList<Node> curr_village = road_graph.get(curr_node.end);
-			visited[curr_node.end] = true;
-
-			for (Node o : curr_village) {
-
-					if (dp_come[o.end] > dp_come[curr_node.end] + o.time) {
-						dp_come[o.end] = dp_come[curr_node.end] + o.time;
-						pq.offer(new Node(o.start, o.end, dp_come[o.end]));
-					}
-
-			}
-		}
-
-		visited = new boolean[students + 1];
-		pq.offer(start_points);
-
-		while (!pq.isEmpty()) {
-
-			Node curr_node = pq.poll();
-			ArrayList<Node> curr_village = road_graph.get(curr_node.start);
-			visited[curr_node.start] = true;
-
-			for (Node o : curr_village) {
-				if (!visited[o.start] && o.end == curr_node.start) {
-					if (dp_go[o.start] > dp_go[curr_node.start] + o.time) {
-						dp_go[o.start] = dp_go[curr_node.start] + o.time;
-						pq.offer(new Node(o.start, o.end, dp_go[o.start]));
-					}
+			int[] curr = pq.poll();
+			int start = curr[0];
+			int time = curr[1];
+			
+			if (go[start] < time) continue;
+			
+			for (int[] n : route1[start]) {
+				if (go[n[0]] > n[1] + time) {
+					go[n[0]] = n[1] + time;
+					pq.add(new int[] {n[0], go[n[0]]});
 				}
 			}
 		}
-
-		for (int i = 1; i < dp_go.length; i++) {
-			answer = Math.max(answer, dp_come[i] + dp_go[i]);
+		
+		pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
+		pq.add(new int[] {x, 0});
+		
+		while (!pq.isEmpty()) {
+			int[] curr = pq.poll();
+			int start = curr[0];
+			int time = curr[1];
+			
+			if (back[start] < time) continue;
+			
+			for (int[] n : route2[start]) {
+				if (back[n[0]] > n[1] + time) {
+					back[n[0]] = n[1] + time;
+					pq.add(new int[] {n[0], back[n[0]]});
+				}
+			}
 		}
-
+		
+		for (int i = 1; i <= n; i++) {
+			answer = Math.max(answer, go[i] + back[i]);
+		}
+		
 		System.out.println(answer);
 	}
 }

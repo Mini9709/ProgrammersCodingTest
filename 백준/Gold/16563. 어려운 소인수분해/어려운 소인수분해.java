9f7df7e031797429c3f0932334d3ab12fc3985e8
Mini9709 <tjdals9709@naver.com>
2026-03-19 15:22:20 +0900

    [Gold IV] Title: 어려운 소인수분해, Time: 4684 ms, Memory: 325876 KB -BaekjoonHub

---
+++ 백준/Gold/16563. 어려운 소인수분해/어려운 소인수분해.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int n;
+	static int maxNum;
+	static int[] nums;
+	static int[] res;
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		n = Integer.parseInt(br.readLine());
+		maxNum = 0;
+		nums = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+			maxNum = Math.max(maxNum, nums[i]);
+		}
+		
+		res = new int[maxNum+1];
+		
+		for (int num = 2; num <= maxNum; num++) {
+			if (res[num] == 0) {
+				res[num] = num;
+				int temp = num * 2;
+				
+				while (temp <= maxNum) {
+					if (res[temp] == 0) {
+						res[temp] = num;
+					}
+					temp += num;
+				}
+			}
+		}
+		
+		
+		for (int a : nums) {
+			int num = a;
+			StringBuilder sb = new StringBuilder();
+			
+			while (num > 1) {
+				sb.append(res[num] + " ");
+				num /= res[num];
+			}
+			
+			System.out.println(sb);
+		}
+	}
+
+}

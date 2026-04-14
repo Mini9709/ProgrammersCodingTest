a99239674f9b8ee1d6e165c1c7fc1823664b90e0
Mini9709 <tjdals9709@naver.com>
2026-04-14 13:43:00 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 5,078 ms, Memory: 106,860 KB -BaekjoonHub

---
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	static int n;
+	static int[][] edges;
+	static int[][] distances;
+	static int answer;
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int testCase = Integer.parseInt(br.readLine());
+		
+		for (int t = 1; t <= testCase; t++) {
+			
+			st = new StringTokenizer(br.readLine());
+			
+			n = Integer.parseInt(st.nextToken());
+			edges = new int[n][n];
+			distances = new int[n][n];
+			
+			for (int i = 0; i < n; i++) {
+				Arrays.fill(distances[i], Integer.MAX_VALUE);
+				distances[i][i] = 0;
+			}
+			
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					edges[i][j] = Integer.parseInt(st.nextToken());
+					
+					if (edges[i][j] == 1) {
+						distances[i][j] = 1;
+					}
+				}
+			}
+
+			
+			for (int x = 0; x < n; x++) {
+				for (int i = 0; i < n; i++) {
+					for (int j = 0; j < n; j++) {
+						if (i == x || j == x || i == j) continue;
+						
+						if (distances[i][x] == Integer.MAX_VALUE || distances[x][j] == Integer.MAX_VALUE) continue;
+						
+						distances[i][j] = Math.min(distances[i][j], distances[i][x] + distances[x][j]);
+					}
+				}
+			}
+			
+			answer = Integer.MAX_VALUE;
+			for (int i = 0; i < n; i++) {
+				int temp = 0;
+				
+				for (int j = 0; j < n; j++) {
+					if (distances[i][j] == Integer.MAX_VALUE) {
+						temp = Integer.MAX_VALUE;
+						break;
+					}
+					
+					temp += distances[i][j];
+				}
+				
+				answer = Math.min(answer, temp);
+			}
+			
+			System.out.println("#" + t + " " + answer);
+		}
+
+	}
+
+}
